47ab26f83e77645365cb4143c9b970b3eafba321
Xeronel <ripster1121@gmail.com>
2016-01-18 11:54:18 -0500

    Print motd

--- bot.py
+++ bot.py
@@ -7,3 +7,6 @@ class Bot(IRC):
                  use_ssl: bool=False, ssl_options: ssl.SSLContext=None,
                  encoding: str='utf-8'):
         super().__init__(host, port, nicks, pwd, use_ssl, ssl_options, encoding)
+
+    def motd(self, message):
+        print(message)
