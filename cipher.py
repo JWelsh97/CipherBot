43ea8613be9b6d80c5304542ba5c639d9fb37aa7
Legacy <legacy11@protonmail.com>
2016-01-18 17:20:07 +0000

    Imported details from config

--- cipher.py
+++ cipher.py
@@ -1,6 +1,12 @@
 from bot import Bot
 import tornado.ioloop
+import config
 
+config = config.read_config()
 if __name__ == '__main__':
-    cli = Bot('affinity.pw', 6697, ['Cipher', 'Cipher_', 'Cipher__'], '', True)
+    cli = Bot(config['irc_host'],
+              config['port'],
+              [config['nickname'], '%s_' % config['nickname'], '%s__' % config['nickname']],
+              config['password'],
+              config['ssl'])
     tornado.ioloop.IOLoop.instance().start()
