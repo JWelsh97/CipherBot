db1bb56101fe5b2f57115f2370a5037bf312159d
Legacy <legacy11@protonmail.com>
2016-01-19 18:14:05 +0000

    Made nickname a list

--- cipher.py
+++ cipher.py
@@ -1,12 +1,12 @@
 from bot import Bot
 import tornado.ioloop
-import config
+from config import read_config
 
-config = config.read_config()
 if __name__ == '__main__':
+    config = read_config()
     cli = Bot(config['irc_host'],
               config['port'],
-              [config['nickname'], '%s_' % config['nickname'], '%s__' % config['nickname']],
+              config['nickname'],
               config['password'],
               config['ssl'])
     tornado.ioloop.IOLoop.instance().start()
