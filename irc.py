3c7ad363ae23d8cb2101469edd3a356d047b9022
Xeronel <ripster1121@gmail.com>
2016-01-20 15:21:35 -0500

    Fixed mistype

--- irc.py
+++ irc.py
@@ -192,7 +192,7 @@ class IRC(object):
         MOTD hanlder
         :param data: MOTD line
         """
-        self.motd(message.strip(b'\r').encode(self.encoding))
+        self.motd(message.strip(b'\r').decode(self.encoding))
 
     def closed(self, data):
         pass
