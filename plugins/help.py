934af759c91bedc6e6eb703ab1eb9cb1e64bfc41
Legacy <legacy11@protonmail.com>
2016-01-30 23:54:39 +0000

    Added mode changed event

--- plugins/help.py
+++ plugins/help.py
@@ -1,14 +1,16 @@
 from cipher.irc import Plugin, Events
+from tornado import gen
 
 
 class Help(Plugin):
     def __init__(self, irc):
         super().__init__(irc)
         self.nickname = self.irc.nicks[0]
-        Events.privmsg += [self.next, self.done, self.idler, self.help]
+        Events.privmsg += [self.next, self.done, self.idler, self.help, self.list]
         Events.join += self.on_join
         Events.part += self.on_part
         Events.quit += self.quit
+        Events.mode_change += self.mode_change
         self.__wait_queue = []
 
     def next(self, source: str, target: str, message: str):
@@ -31,8 +33,8 @@ class Help(Plugin):
                     return
                 user = message[1]
                 if user not in self.__wait_queue:
-                    data = 'KICK #help %s %s' % (user, "You have been dealt with, \
-                           if you have any other queries visit #help again.")
+                    data = 'KICK #help %s %s' % (user, 'You have been dealt with, \
+                           if you have any other queries visit #help again.')
                     self.irc.send(data)
                 else:
                     return
@@ -47,8 +49,8 @@ class Help(Plugin):
                     return
                 user = message[1]
                 if user not in self.__wait_queue:
-                    data = 'KICK #help %s %s' % (user, "There is no idling in this channel, if and when \
-                           you come back, be sure to join #help again")
+                    data = 'KICK #help %s %s' % (user, 'There is no idling in this channel, if and when \
+                           you come back, be sure to join #help again')
                     self.irc.send(data)
                 else:
                     return
@@ -69,21 +71,44 @@ class Help(Plugin):
                     data = 'SAJOIN %s #help' % user
                     self.irc.send(data)
                 else:
-                    self.irc.send("PRIVMSG #tgif You do not have permission to use this command.")
+                    self.irc.send('PRIVMSG #tgif You do not have permission to use this command.')
 
+    def list(self, source: str, target: str, message: str):
+        if not target.startswith('#'):
+            return
+        if target == '#help' and message == '!list':
+            if not self.user_mode(source, target) in ['~', '&', '@']:
+                self.irc.send('PRIVMSG #help You do not have permission to use this command.')
+                return
+
+            if not self.__wait_queue:
+                self.irc.send('PRIVMSG #help Nobody is waiting.')
+                return
+            else:
+                data = ' '.join(['%s: %s' % (x + 1, y) for x, y in enumerate(self.__wait_queue)])
+                self.irc.send('PRIVMSG #help %s' % data)
+
+    @gen.coroutine
     def on_join(self, user, channel):
         if channel == '#help':
             if user != self.nickname:
-                if self.user_mode(user, channel) == '':
-                    self.send_notice(user, "Please wait to be voiced, \
-                                           then you will be seen to.")
-                    self.__wait_queue.append(user)
+                self.__wait_queue.append(user)
+                yield gen.sleep(1)
+                if user in self.__wait_queue:
+                    self.send_notice(user, 'Please wait to be voiced, \
+                                           then you will be seen to.')
 
     def on_part(self, user, channel, message):
         if channel == '#help':
             if user in self.__wait_queue:
                 self.__wait_queue.remove(user)
 
-    def quit(self, nickname):
-        if nickname in self.__wait_queue:
-            self.__wait_queue.remove(nickname)
+    def quit(self, user):
+        if user in self.__wait_queue:
+            self.__wait_queue.remove(user)
+
+    def mode_change(self, channel, mode, target):
+        if channel == '#help':
+            if target in self.__wait_queue:
+                if mode in ['+qo', '+ao', '+o']:
+                    self.__wait_queue.remove(target)
